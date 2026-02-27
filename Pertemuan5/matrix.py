5ad185e176974793daf81ab4d7bc5c0562e18a84
maghfiraathiyahadhavi <maghfira.athiyah6717@student.unri.ac.id>
2026-02-27 14:51:51 +0700

    selesai mengerjakan tugas

---
+++ Pertemuan5/matrix.py
@@ -0,0 +1,13 @@
+matrix = [[0, 0, 0],
+          [0, 1, 0],
+          [0, 3, 2]] 
+
+#cara akses 
+print(matrix[2][1])
+
+#Transpose matrix
+A = [[1, 2, 3],
+     [2, 3, 4]]
+
+list(zip(*A))
+print(A)
